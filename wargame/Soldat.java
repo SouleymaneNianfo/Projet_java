58f095f99ac200db3ef847d5c6bc0436c859ae46
Nianfo <snianfo@gmail.com>
2016-11-12 18:22:11 +0100

    le projet java

---
+++ wargame/Soldat.java
@@ -0,0 +1,22 @@
+package wargame;
+
+public class Soldat extends Position implements ISoldat{
+
+Heros[] heros;
+	public Soldat(){
+		
+		heros= new Heros[7];
+	}
+public int getPoints() { return this.getPoints(); }
+public int getPortee() { return this.getPortee(); }
+
+public	int getTour(){return 0;}
+
+public void joueTour(int tour){}
+public void combat(Soldat soldat){
+	
+}
+public void seDeplace(Position newPos){
+	
+}
+}
